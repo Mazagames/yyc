3be304cbd546e980a7d9ec4d19c98f56790a5af4
Mazagames <80614728+Mazagames@users.noreply.github.com>
2021-12-15 23:13:51 +0530

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,30 @@
+
+
+
+const scriptsInEvents = {
+
+		async EventSheet1_Event3_Act3(runtime, localVars)
+		{
+			var url = "https://gameapi.cosmicegg.tech/gameapi.aspx?fn=GetGameToken";
+			
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", url);
+			
+			xhr.setRequestHeader("Authorization", "a44dfa90fb2b4f4e98f7b9149cc1bf3807b7628465dd9d467c58cf0fcbd9354f");
+			xhr.setRequestHeader("Content-Type", "text/plain");
+			
+			xhr.onreadystatechange = function () {
+			   if (xhr.readyState === 4) {
+			      console.log(xhr.status);
+			      console.log(xhr.responseText);
+			   }};
+			
+			var data = "1639576817476";
+			
+			xhr.send(data);
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
